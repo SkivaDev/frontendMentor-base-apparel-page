814a007eb2c88010dd43cbc412b292bc817e70cd
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2024-02-02 10:48:12 -0500

    feat(project): :art: add Tailwind styles to image card

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -12,6 +12,11 @@ export default {
 				'desaturated-red': 'hsl(0, 36%, 70%)',
 				'soft-red': 'hsl(0, 93%, 68%)',
 				'dark-grayish-red': 'hsl(0, 6%, 24%)'
+			},
+			borderColor: {
+				'desaturated-red': 'hsl(0, 36%, 70%)',
+				'soft-red': 'hsl(0, 93%, 68%)',
+				'dark-grayish-red': 'hsl(0, 6%, 24%)'
 			}
 		},
 	},
