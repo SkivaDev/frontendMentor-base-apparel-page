b391c7cc115ddece1f75ab75062e8c41b5c3e004
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2024-02-05 09:37:26 -0500

    feat(project): :lipstick: resizing models and editing styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -17,7 +17,15 @@ export default {
 				'desaturated-red': 'hsl(0, 36%, 70%)',
 				'soft-red': 'hsl(0, 93%, 68%)',
 				'dark-grayish-red': 'hsl(0, 6%, 24%)'
-			}
+			},
+			backgroundImage: {
+				'pattern': 'url("/images/bg-pattern-desktop.svg")'
+			},
+			boxShadow: {
+				'button-shadow': '0px 0px 18px 1px hsl(0, 20%, 80%)'
+			},
+			
+			
 		},
 	},
 	plugins: [],
